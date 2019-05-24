61773171cfe563813341bcbe68f153be31992611
amgclub <31364804+amgclub@users.noreply.github.com>
2019-05-24 13:07:19 +1000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'armadilloliberate') {
+    msg.reply('Passcode is fully redeemed.');
+  }
+});
+
+client.login(auth.token);
