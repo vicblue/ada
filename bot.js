68504a21737c58f8ad21c70fdcf0e63335a5dbde
oscarc134 <31364804+oscarc134@users.noreply.github.com>
2019-05-24 13:41:00 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
-const auth = require('./auth.json');
+
+ 
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+
+    console.log('I am ready!');
+
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
 
-client.on('message', msg => {
-  if (msg.content === 'armadilloliberate') {
-    msg.reply('Passcode is fully redeemed.');
-  }
-});
+ 
 
-client.login(auth.token);
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
